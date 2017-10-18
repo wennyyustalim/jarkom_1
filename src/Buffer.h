55039ecf36f3e7e5ac384ac9b2d0f01ee918bb06
Daniel <nieltansah@gmail.com>
2017-10-18 09:10:53 +0700

    Use dynamic allocation for Buffer model

--- src/Buffer.h
+++ src/Buffer.h
@@ -1,28 +1,84 @@
 #pragma once
 
-#include <bitset>
+#include <algorithm>
 #include <cstdint>
 #include <cstdlib>
 #include <cstring>
 
+template<typename T>
 class Buffer {
 public:
-    Buffer (void) {
-        memset (data, 0, sizeof (data));
+    struct Item {
+        T data;
+        bool fin;
+    };
+
+    // Constructor.
+
+    Buffer (size_t _size = 256)
+        : size (_size) {
+        data = new Item[size] ();
     }
 
-    static const size_t size = 256;
+    Buffer (const Buffer& _src) = delete;
+    Buffer& operator= (const Buffer& _src) = delete;
 
-    char* slide (uint32_t _seq_num);
+    virtual ~Buffer (void) {
+        delete data;
+    }
+
+    // Slide.
+
+    T* slide (uint32_t _seq_num) {
+        bool allowed = false;
+        T* data_ptr = nullptr;
+        
+        // Fix window size if invalid.
+        win_size = std::min (win_size, data_size);
+    
+        if (win_size > 0) {
+            uint32_t laf = cur_seq_num + win_size;
+    
+            if (cur_seq_num > laf) {
+                allowed = (_seq_num >= cur_seq_num) || (_seq_num < laf);
+            } else {
+                allowed = (_seq_num >= cur_seq_num) && (_seq_num < laf);
+            }
+        }
+    
+        if (allowed) {
+            size_t i_win = _seq_num - cur_seq_num;
+            size_t i_buf = (win_begin + i_win) % size;
+    
+            // Accept frame.
+            Item& item = data[i_buf];
+    
+            data_ptr = &item.data;
+            item.fin = true;
+    
+            // Shift if needed.
+            while (data[win_begin].fin) {
+                data[win_begin].fin = false;
+    
+                // Advance everything.
+                cur_seq_num += 1;
+                win_begin = (win_begin + 1) % size;
+    
+                data_size--;
+            }
+        }
+    
+        return data_ptr;
+    }
+
+    const size_t size;
 
     uint32_t cur_seq_num = 0;
 
-    char data[size];
-    size_t data_size = 0;
+    Item* data;
 
-    size_t pref_win_size = 0;
+    size_t data_size = 0;
 
     size_t win_begin = 0;
     size_t win_size = 0;
-    std::bitset<size> win_flag;
 };
